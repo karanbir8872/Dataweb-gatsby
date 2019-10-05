5b093f6fca352ed0f482b9fe829a158465351fdd
karanbir8872 <wwwwkaranbir786@gmail.com>
2019-10-05 11:32:22 +0530

    few-changes

--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Card from './card';
+import Card from './Card';
 
 const Section = styled.div`
   background-color: #0c0f38;
