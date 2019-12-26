411375507d7e4bd0a0c3d0d7fca874de3c2722eb
karanbir8872 <wwwkaranbir786@gmail.com>
2019-12-26 09:25:37 +0530

    added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,17 +9,58 @@ const Section = styled.div`
   .container {
     margin-top: 4rem;
   }
-  p {
-    margin-bottom: 1rem;
+  .padd {
+    padding: 1.5rem;
   }
-  .image {
-    width: 500px;
-    height: auto;
-    margin: 0 auto;
-    object-position: center;
+  .host {
+    font-size: 57px;
+    font-family: archivo, sans-serif;
+    line-height: 1.3;
+    margin-bottom: 3rem;
   }
-  .button {
+  .main {
+    margin-top: 5rem;
+  }
+  .mgg {
+    margin-right: 15px;
+    color: blue;
+    font-size: 18px;
+  }
+  .icontext {
+    font-size: 21px;
+    margin-bottom: 17px;
+  }
+  .maincontent {
+    margin-top: 10rem;
+  }
+  .mark {
+    font-size: 25px;
+    margin-left: 5rem;
     margin-top: 2rem;
+  }
+  .anderson {
+    margin-left: 5rem;
+    line-height: 2;
+    font-size: 15px;
+  }
+  .web {
+    padding: 16px;
+    font-size: 20px;
+    line-height: 1.4;
+  }
+  .imageas {
+    margin-top: 43px;
+    margin-left: 17px;
+    height: 132px;
+    width: 407px;
+  }
+  .tle {
+    line-height: 1.5;
+    font-size: 28px;
+    margin-top: 2rem;
+  }
+  .iner {
+    margin-top: 4rem;
   }
 `;
 
@@ -28,47 +69,52 @@ const Contact = () => (
     <Seo title="Contact Us" />
     <Section className="section">
       <div className="container">
-        <div className="columns is-centered">
-          <div className="column is-two-fifths">
-            <h2 className="title is-1 has-text-weight-bold">Contact Us</h2>
-            <p>
-              We’re as accessible as your good neighbour. Feel free
-              <br />
-              to give us a shout.
+        <p className="tit has-text-weight-bold margin has-text-centered is-size-1">
+          Welcome To Hami
+        </p>
+        <p className="padd line has-text-centered is-size-4">
+          Deploy your service infrastructure on our fully redundant, high
+          performance cloud platform and benefit from its high reliability,
+          security and enterprise feature set. Easily enhance the performance,
+          security and reliability of your services with one of our managed
+          cloud hosting products, free data migration included.
+        </p>
+        <div className="columns main is-centered is-variable is-8">
+          <div className="column is-half">
+            <img
+              src="https://colorlib.com/preview/theme/hami/img/bg-img/4.jpg"
+              alt="contact us"
+            />
+          </div>
+          <div className="column is-half">
+            <p className="host">
+              We Offer Professional Affordable Web Hosting & Related Services
             </p>
-            <p>
-              <span role="img" aria-label="Round Pushpin">
-                📍
-              </span>{' '}
-              {config.location}
+            <p className="has-text-black icontext">
+              <i className="fas fa-check mgg" />
+              Reliability, Speed and Security
             </p>
-            <p className="is-4">
-              <span role="img" aria-label="e-mail">
-                📧
-              </span>{' '}
-              <a href={`mailto:${config.email}`}>{config.email}</a>
+            <p className="has-text-black icontext">
+              <i className="fas fa-check mgg " />
+              Free Web Tools & Applications
             </p>
-            <p>
-              <span role="img" aria-label="telephone">
-                ☎️
-              </span>{' '}
-              <a href={`tel:${config.telephone}`}>{config.telephone}</a>
-            </p>
-            <p className="control">
-              <a href="#">
-                <button type="submit" className="button is-secondary is-medium">
-                  Get in touch
-                </button>
-              </a>
+            <p className="has-text-black icontext">
+              <i className="fas fa-check mgg" />
+              24/7 Award Winning Support
             </p>
           </div>
-          <div className="column is-two-fifths">
-            <img className="image" src="/images/contact.svg" alt="contact us" />
-          </div>
+        </div>
+        <div className="maincontent">
+          <p className="tit has-text-weight-bold margin has-text-centered is-size-1">
+            Our Management Team
+          </p>
+          <p className="padd line has-text-centered is-size-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
         </div>
       </div>
     </Section>
   </Layout>
 );
-
 export default Contact;
