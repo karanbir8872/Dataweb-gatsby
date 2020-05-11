0d1d117370ff3a7364521b82786cc09eea346011
karanbir8872 <wwwkaranbir786@gmail.com>
2020-05-11 22:10:37 +0530

    minor fixes in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,55 +13,148 @@ const Section = styled.div`
   .fild {
     padding: 2rem;
   }
+  .columns {
+    display: unset;
+  }
+  button {
+    margin-top: 2rem;
+  }
 `;
 
-const Form = () => (
-  <Section className="section">
-    <div className="container">
-      <div className="columns">
-        <div
-          className="column colr is-half
-is-offset-one-quarter has-text-centered"
-        >
-          <div className="box">
-            <div className="fild">
-              <div className="field">
-                <div className="control">
-                  <input
-                    className="input is-medium"
-                    type="text"
-                    placeholder="Name"
-                  />
-                </div>
-              </div>
+const encode = data => {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+};
 
-              <div className="field">
-                <div className="control">
-                  <input
-                    className="input is-medium"
-                    type="text"
-                    placeholder="Email"
-                  />
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', phone: '', company: '', message: '' };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => {
+        this.setState({
+          name: '',
+          email: '',
+          phone: '',
+          company: '',
+          message: '',
+        });
+        alert('Success!');
+      })
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, phone, company, message } = this.state;
+    return (
+      <Section className="section">
+        <div className="container">
+          <div className="columns">
+            <form
+              data-netlify="true"
+              name="contact"
+              onSubmit={this.handleSubmit}
+            >
+              <div
+                className="column colr is-half
+is-offset-one-quarter has-text-centered"
+              >
+                <div className="box">
+                  <div className="fild">
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          className="input is-medium"
+                          type="text"
+                          placeholder="Name"
+                          value={name}
+                          name="name"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          className="input is-medium"
+                          type="text"
+                          placeholder="Email"
+                          value={email}
+                          name="email"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          name="company"
+                          className="input is-medium "
+                          type="text"
+                          value={company}
+                          placeholder="Your company name"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control">
+                        <input
+                          name="phone"
+                          className="input is-medium"
+                          placeholder="Phone"
+                          type="tel"
+                          value={phone}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <div className="control is-medium">
+                        <textarea
+                          name="message"
+                          value={message}
+                          className="textarea is-medium is-radiusless"
+                          placeholder="Your message"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <p className="has-text-white is-size-6">
+                      In submitting this form you are agreeing to our
+                      <span className="has-text-white has-text-weight-bold">
+                        {'   '}terms and conditions
+                      </span>
+                    </p>
+                    <button
+                      type="submit"
+                      className="button is-medium has-text-info"
+                    >
+                      <span className="is-size-4">Submit</span>
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className="field">
-                <div className="control">
-                  <input
-                    className="input is-medium"
-                    type="text"
-                    placeholder="Phone"
-                  />
-                </div>
-              </div>
-              <textarea className="textarea is-medium" placeholder="Message" />
-              <a className="button is-rounded is-medium margin is-size-3">
-                Send
-              </a>
-            </div>
+            </form>
           </div>
         </div>
-      </div>
-    </div>
-  </Section>
-);
-export default Form;
+      </Section>
+    );
+  }
+}
+export default ContactForm;
